6f5a7af2e9d90f940d660b907520b8a0ddfdc481
Addy <adelaide.bornn@gmail.com>
2026-04-02 22:25:51 -0700

    Refactor traffic light code so that the lights work at different places and times and modes
    AHHHHHHHHHHHHHHHHHH

--- traffic.ino
+++ traffic.ino
@@ -1,104 +1,76 @@
-// C++ code
-// LED pins
-int redPin = 3;
-int greenPin = 5;
-int bluePin = 6;
+// TOP light
+int r1=3, g1=5, b1=6;
 
-// Button pins
-int redBtn = 2;
-int yellowBtn = 4;
-int greenBtn = 7;
-int allBtn = 8;
-int whiteBtn = 9;
+// MIDDLE light
+int r2=10, g2=11, b2=12;
 
-// Mode tracking
-int currentMode = 0;
+// BOTTOM light
+int r3=A0, g3=A1, b3=A2;
+
+// Buttons
+int redBtn=2, yellowBtn=4, greenBtn=7, allBtn=8, whiteBtn=9;
+
+int mode=0;
 
 void setup() {
-pinMode(redPin, OUTPUT);
-pinMode(greenPin, OUTPUT);
-pinMode(bluePin, OUTPUT);
+pinMode(r1,OUTPUT); pinMode(g1,OUTPUT); pinMode(b1,OUTPUT);
+pinMode(r2,OUTPUT); pinMode(g2,OUTPUT); pinMode(b2,OUTPUT);
+pinMode(r3,OUTPUT); pinMode(g3,OUTPUT); pinMode(b3,OUTPUT);
 
-pinMode(redBtn, INPUT_PULLUP);
-pinMode(yellowBtn, INPUT_PULLUP);
-pinMode(greenBtn, INPUT_PULLUP);
-pinMode(allBtn, INPUT_PULLUP);
-pinMode(whiteBtn, INPUT_PULLUP);
+pinMode(redBtn,INPUT_PULLUP);
+pinMode(yellowBtn,INPUT_PULLUP);
+pinMode(greenBtn,INPUT_PULLUP);
+pinMode(allBtn,INPUT_PULLUP);
+pinMode(whiteBtn,INPUT_PULLUP);
 }
 
 void loop() {
 
-if (digitalRead(redBtn) == LOW) {
-toggleMode(1);
-delay(200);
-}
-
-if (digitalRead(yellowBtn) == LOW) {
-toggleMode(2);
-delay(200);
-}
-
-if (digitalRead(greenBtn) == LOW) {
-toggleMode(3);
-delay(200);
-}
-
-if (digitalRead(allBtn) == LOW) {
-toggleMode(4);
-delay(200);
-}
-
-if (digitalRead(whiteBtn) == LOW) {
-toggleMode(5);
-delay(200);
-}
+if(digitalRead(redBtn)==LOW){toggle(1); delay(200);}
+if(digitalRead(yellowBtn)==LOW){toggle(2); delay(200);}
+if(digitalRead(greenBtn)==LOW){toggle(3); delay(200);}
+if(digitalRead(allBtn)==LOW){toggle(4); delay(200);}
+if(digitalRead(whiteBtn)==LOW){toggle(5); delay(200);}
 
 updateLights();
 }
 
-// Toggle logic
-void toggleMode(int mode) {
-if (currentMode == mode) {
-currentMode = 0; // turn off
-} else {
-currentMode = mode;
-}
+void toggle(int m){
+if(mode==m) mode=0;
+else mode=m;
 }
 
-// Set colors
-void updateLights() {
+void offAll(){
+setColor(r1,g1,b1,0,0,0);
+setColor(r2,g2,b2,0,0,0);
+setColor(r3,g3,b3,0,0,0);
+}
 
-switch(currentMode) {
+void updateLights(){
 
-case 0: // OFF
-setColor(0, 0, 0);
+offAll();
+
+switch(mode){
+case 1: setColor(r1,g1,b1,255,0,0); break;
+case 2: setColor(r2,g2,b2,255,150,0); break;
+case 3: setColor(r3,g3,b3,0,255,0); break;
+
+case 4:
+setColor(r1,g1,b1,255,0,0);
+setColor(r2,g2,b2,255,150,0);
+setColor(r3,g3,b3,0,255,0);
 break;
 
-case 1: // RED
-setColor(255, 0, 0);
-break;
-
-case 2: // YELLOW
-setColor(255, 150, 0);
-break;
-
-case 3: // GREEN
-setColor(0, 255, 0);
-break;
-
-case 4: // ALL (traffic light look)
-setColor(255, 255, 0);
-break;
-
-case 5: // WHITE
-setColor(255, 255, 255);
+case 5:
+setColor(r1,g1,b1,255,255,255);
+setColor(r2,g2,b2,255,255,255);
+setColor(r3,g3,b3,255,255,255);
 break;
 }
 }
 
-// RGB control
-void setColor(int r, int g, int b) {
-analogWrite(redPin, r);
-analogWrite(greenPin, g);
-analogWrite(bluePin, b);
+void setColor(int rPin,int gPin,int bPin,int r,int g,int b){
+analogWrite(rPin,r);
+analogWrite(gPin,g);
+analogWrite(bPin,b);
 }
